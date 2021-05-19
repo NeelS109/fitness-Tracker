c2cabc2751310bbdcea9df30971e444c72997061
Neel Sheth <nsheth109@gmail.com>
2021-05-19 13:16:19 -0400

    modified server.js to req4uire morgan

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const { db } = require("./models/workout");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+
+app.use(require('./routes/htmlRoutes'));
+app.use(require("./routes/apiRoutes"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
